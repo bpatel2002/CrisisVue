716edef14e775d3b1466d0f8a2d8413061403aee
Arjit Singh <arjitsingh@Arjits-MacBook-Pro-2.local>
2023-09-28 14:53:05 -0400

    Mass Shooting Event Component

---
+++ client/frontend/src/app/components/MassShootingEvent.tsx
@@ -0,0 +1,32 @@
+// components/MassShootingEvent.tsx
+
+import React from 'react';
+
+interface MassShootingEventProps {
+  event: string;
+  date: string;
+  perpetrator: string;
+  location: string;
+  numVictims: number;
+
+}
+
+const MassShootingEvent: React.FC<MassShootingEventProps> = ({
+  event,
+  date,
+  perpetrator,
+  location,
+  numVictims,
+}) => {
+  return (
+    <div>
+      <h2>{event}</h2>
+      <p>Date: {date}</p>
+      <p>Perpetrator: {perpetrator}</p>
+      <p>Location: {location}</p>
+      <p>Number of Victims: {numVictims}</p>
+    </div>
+  );
+};
+
+export default MassShootingEvent; 
