7d1d86b41fbfb01675e1c82210f7615d18a398c8
bpatel21 <bpatel21@vt.edu>
2023-10-03 16:18:28 -0400

    Working homepage api call with axios, not formatted correctly in card component

--- client/frontend/src/app/page.tsx
+++ client/frontend/src/app/page.tsx
@@ -1,197 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
 import React from 'react';
 import MassShootingEvent from './components/massShootingEvent';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from '../../node_modules/axios/index';
 
 export default function Home() {
+  const [events, setEvents] = useState<any[]>([]);
+
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/events")
+      .then(response => {
+        setEvents(response.data.data);
+      
+      })
+      
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-          
+    <div>
+      {events.map((event, index) => (
+        <MassShootingEvent
+          key={index}
+          event={event.event}
+          date={event.date}
+          perpetrator={event.perpetrator}
+          location={event.location}
+          numVictims={event.numVictims}
         />
-      </div>
-
-      <div className={styles.grid}>
-
-        <a
-          //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div>
-            {/* <h1>My Next.js App</h1> */}
-            <MassShootingEvent
-            event="Sandy Hook"
-            date="October 12, 2020"
-            perpetrator="John Doe"
-            location="City XYZ"
-            numVictims={10}
-            />
-          </div>
-        </a>
-
-        <a
-          //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          <div>
-            {/* <h1>My Next.js App</h1> */}
-            <MassShootingEvent
-            event="Las Vegas Strip"
-            date="October 12, 2020"
-            perpetrator="John Doe"
-            location="City XYZ"
-            numVictims={10}
-            />
-          </div>
-        </a>
-
-        <a
-          //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          <div>
-            {/* <h1>My Next.js App</h1> */}
-            <MassShootingEvent
-            event="Virgnia Tech"
-            date="October 12, 2020"
-            perpetrator="John Doe"
-            location="City XYZ"
-            numVictims={10}
-            />
-          </div>
-        </a>
-
-        <a
-          //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          <div>
-            {/* <h1>My Next.js App</h1> */}
-            <MassShootingEvent
-            event="Robb Elementary"
-            date="October 12, 2020"
-            perpetrator="John Doe"
-            location="City XYZ"
-            numVictims={10}
-            />
-          </div>
-        </a>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          <div>
-            {/* <h1>My Next.js App</h1> */}
-            <MassShootingEvent
-            event="San Bernardino"
-            date="October 12, 2020"
-            perpetrator="John Doe"
-            location="City XYZ"
-            numVictims={10}
-            />
-          </div>
-        </a>
-
-        <a
-          //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          <div>
-            {/* <h1>My Next.js App</h1> */}
-            <MassShootingEvent
-            event="Orlando nightclub"
-            date="October 12, 2020"
-            perpetrator="John Doe"
-            location="City XYZ"
-            numVictims={10}
-            />
-          </div>
-        </a>
-
-        <a
-          //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          <div>
-            {/* <h1>My Next.js App</h1> */}
-            <MassShootingEvent
-            event="Texas Baptist Church"
-            date="October 12, 2020"
-            perpetrator="John Doe"
-            location="City XYZ"
-            numVictims={10}
-            />
-          </div>
-        </a>
-
-        <a
-          //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          <div>
-            {/* <h1>My Next.js App</h1> */}
-            <MassShootingEvent
-            event="El Paso"
-            date="October 12, 2020"
-            perpetrator="John Doe"
-            location="City XYZ"
-            numVictims={10}
-            />
-          </div>
-        </a>
-      </div>
-    </main>
-  )
+      ))}
+    </div>
+  );
 }
