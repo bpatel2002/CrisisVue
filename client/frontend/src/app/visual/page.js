a67bc1bf91bb5f30678ef0c5638162508ff23463
harrisn24 <harrisn24@vt.edu>
2023-10-07 14:58:35 -0400

    visualization page now updates chart based on data from backend

--- client/frontend/src/app/visual/page.js
+++ client/frontend/src/app/visual/page.js
@@ -2,14 +2,26 @@
 import React from 'react'
 import BarChart from './components/barchart'
 import axios from 'axios'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import'./visual.css'
 function page() {
-  //axios some data and build data
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/events")
+      .then(response => {
+        setEvents(response.data.data);
+      })   
+  }, []);
+  const labels = events.map(event => event.event_name);
+  const casualties = events.map(event => event.casualties);
   const data = {
-    labels: ['Cal', 'Ido',],
+    labels: labels,
     datasets: [
       {
         label: 'Casualties',
-        data: [1, 2],
+        data: casualties,
         backgroundColor: [
           'rgba(234, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
@@ -24,14 +36,32 @@ function page() {
     ],
   };
 
-  async function getShootings() {
-    var result = await axios.get("http://127.0.0.1:5000/events");
-    console.log(result.data);
-  }
 
-  getShootings();
+  // async function getShootings() {
+  //   var result = await axios.get("http://127.0.0.1:5000/events");   
+  // }
   return (
+    <>
+    <div style={{display: 'flex', gap: '20px', marginTop: '2em'}}>
     <div style = {{height: '20em', width: '40em'}}><BarChart chartData={data}/></div>
+    <div className= "eventList">
+            {events.map((event, index) => (
+                <div key={index} className="eventCard">
+                    <div className="eventDetails">
+                        <div className="eventTitle">{event.event_name}</div>
+                        <div className="eventDate">Date: {event.date}</div>
+                        <div className="eventDescription">{event.summary}</div>
+                        <div className='location'>Location: {event.location}</div>
+                        <div className="eventFooter">
+
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+
+    </div>
+    </>
   );
 }
 
