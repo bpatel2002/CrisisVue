600b10a3f4a4e1aaa23fb72721640bf8d5ab421b
Dong Xiao <dong99x@Dongs-MacBook-Air.local>
2023-10-12 16:40:07 -0400

    update the field of formData, 200 response tested

--- client/frontend/src/app/submit/page.tsx
+++ client/frontend/src/app/submit/page.tsx
@@ -1,69 +1,76 @@
-"use client";
-import React, { useState } from "react";
-import "./SubmitPage.css";
-import axios from "axios";
+'use client';
+import React, { useState } from 'react';
+import './SubmitPage.css';
+import axios from 'axios'
 
 function SubmitPage() {
-  const [perpetrator, setPerpetrator] = useState("");
-  const [location, setLocation] = useState("");
-  const [summary, setSummary] = useState("");
-  const [date, setDate] = useState("");
-  const [motive, setMotive] = useState("");
+  const [name, setName] = useState('');
+  const [perpetrator, setPerpetrator] = useState('');
+  const [summary, setSummary] = useState('');
+  const [date, setDate] = useState('');
+  const [location, setLocation] = useState('');
+  const [motive, setMotive] = useState('');
+  const [casualties, setCasualties] = useState('');
   const [image, setImage] = useState<File | null>(null);
-  const [url, setUrl] = useState("");
+  const [url, setUrl] = useState('');
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
 
-    // Create a data object with the form data
-    const formData = {
-      perpetrator: perpetrator,
-      location: location,
-      summary: summary,
-      data: date,
-      motive: motive,
-      url: url,
-    };
+const handleSubmit = async (e: React.FormEvent) => {
+  e.preventDefault();
 
-    // const formData = new FormData();
-    // formData.append('perpetrator', perpetrator);
-    // formData.append('location', location);
-    // formData.append('summary', summary);
-    // formData.append('date', date);
-    // formData.append('motive', motive);
-    // if (image) {
-    //   formData.append('image', image);
-    // }
-    // formData.append('url', url);
-
-    try {
-      // Make a POST request using Axios
-      const response = await axios.post(
-        "http://127.0.0.1:5000/submitForm",
-        formData
-      );
-
-      // Handle the response (you can display a success message, reset the form, etc.)
-      console.log("Submission successful:", response.data);
-
-      // Clear the form fields after successful submission (optional)
-      setPerpetrator("");
-      setLocation("");
-      setSummary("");
-      setDate("");
-      setMotive("");
-      setImage(null);
-      setUrl("");
-    } catch (error) {
-      // Handle errors (you can display an error message to the user)
-      console.error("Submission error:", error);
-    }
+  // Create a data object with the form data
+  const formData = {
+    name: name,
+    perpetrator: perpetrator,
+    summary: summary,
+    date: date,
+    location: location,
+    motive: motive,
+    casualties: casualties,
+    additional_fields: url,
   };
+
+
+  try {
+    // Make a POST request using Axios
+    const response = await axios.post('http://127.0.0.1:5000/submitForm', formData);
+
+    // Handle the response (you can display a success message, reset the form, etc.)
+    console.log('Submission successful:', response.data);
+
+    // Clear the form fields after successful submission (optional)
+    setName('');
+    setPerpetrator('');
+    setSummary('');
+    setDate('');
+    setLocation('');
+    setMotive('');
+    setCasualties('');
+    setImage(null);
+    setUrl('');
+  } catch (error) {
+    // Handle errors (you can display an error message to the user)
+    console.error('Submission error:', error);
+  }
+};
+
 
   return (
     <div className="form-container">
       <h1>Submit Information</h1>
       <form onSubmit={handleSubmit}>
+
+        <label htmlFor="name">name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <br />
+
         <label htmlFor="perpetrator">Perpetrator:</label>
         <input
           type="text"
@@ -75,17 +82,7 @@ function SubmitPage() {
         />
         <br />
 
-        <label htmlFor="location">Location:</label>
-        <input
-          type="text"
-          id="location"
-          name="location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          required
-        />
-        <br />
-
+       
         <label htmlFor="summary">Summary:</label>
         <textarea
           id="summary"
@@ -104,6 +101,17 @@ function SubmitPage() {
           name="date"
           value={date}
           onChange={(e) => setDate(e.target.value)}
+          required
+        />
+        <br />
+
+        <label htmlFor="location">Location:</label>
+        <input
+          type="text"
+          id="location"
+          name="location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
           required
         />
         <br />
@@ -128,6 +136,17 @@ function SubmitPage() {
         />
         <br />
 
+        <label htmlFor="casualties">Casualties:</label>
+        <input
+          type="text"
+          id="casualties"
+          name="casualties"
+          value={casualties}
+          onChange={(e) => setCasualties(e.target.value)}
+          required
+        />
+        <br />
+
         <label htmlFor="url">Related URLs (comma-separated):</label>
         <input
           type="text"
@@ -145,3 +164,4 @@ function SubmitPage() {
 }
 
 export default SubmitPage;
+
