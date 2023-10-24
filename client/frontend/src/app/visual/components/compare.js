9b141f7c7ed096460f3631a8b491a91aace7f22c
harrisn24 <harrisn24@vt.edu>
2023-10-24 13:18:56 -0400

    compare update

---
+++ client/frontend/src/app/visual/components/compare.js
@@ -0,0 +1,29 @@
+'use client'
+import React, { Component } from 'react';
+import MassShootingEvent from "../../components/MassShootingEvent";
+import '../visual.css'
+
+const Compare = ({ Data }) => {
+    return (
+        <>
+        <div style={{display: 'flex'}}>
+        {Data.map((event, index) => (
+            <div className='eventCard'>
+            <MassShootingEvent
+              key={index} // Ensure each component maintains a unique key for optimal rendering performance
+              id = {event._id.$oid}
+              event={event.event_name}
+              date={event.date || "N/A"}
+              perpetrator={event.perpetrator || "N/A"}
+              location={event.location || "N/A"}
+              numVictims={event.casualties || 0}
+            />
+            </div>
+          ))}
+        </div>
+        </>
+
+    )
+}
+
+export default Compare
--- client/frontend/src/app/visual/page.js
+++ client/frontend/src/app/visual/page.js
@@ -4,124 +4,135 @@ import BarChart from './components/barchart'
 import axios from 'axios'
 import { useState } from 'react';
 import { useEffect } from 'react';
-import'./visual.css'
-import { OrganizeImportsMode } from 'typescript';
+import './visual.css'
+import Compare from './components/compare'
 function page() {
-  const [events, setEvents] = useState([]);
-  const [selectedEvents, setSelectedEvents] = useState([]);
-  const [originalChartData, setOriginalChartData] = useState([]);
+    const [events, setEvents] = useState([]);
+    const [selectedEvents, setSelectedEvents] = useState([]);
+    const [originalChartData, setOriginalChartData] = useState([]);
+    const [compareData, setCompareData] = useState([]);
 
 
-const handleEventSelection = (event) => {
-  let updatedSelection = [...selectedEvents];
-  if (updatedSelection.includes(event)) {
-      // If the event is already selected, deselect it
-      updatedSelection = updatedSelection.filter(e => e !== event);
-  } else {
-      // If 2 events are already selected, remove the first one (oldest selection)
-      if (updatedSelection.length === 2) {
-          updatedSelection.shift();
-      }
-      updatedSelection.push(event);
-  }
-  setSelectedEvents(updatedSelection);
-};
+    const handleEventSelection = (event) => {
+        let updatedSelection = [...selectedEvents];
+        if (updatedSelection.includes(event)) {
+            // If the event is already selected, deselect it
+            updatedSelection = updatedSelection.filter(e => e !== event);
+        } else {
+            // If 2 events are already selected, remove the first one (oldest selection)
+            if (updatedSelection.length === 2) {
+                updatedSelection.shift();
+            }
+            updatedSelection.push(event);
+        }
+        setSelectedEvents(updatedSelection);
+    };
 
 
 
-const handleCompare = () => {
-  const selectedLabels = selectedEvents.map(event => event.event_name);
-  const selectedCasualties = selectedEvents.map(event => event.casualties);
-  
-  const newChartData = {
-      labels: selectedLabels,
-      datasets: [
-          {
-              label: 'Casualties',
-              data: selectedCasualties,
-              backgroundColor: [
-                  'rgba(234, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  // ... you can add more colors if you have more datasets ...
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  // ... corresponding border colors ...
-              ],
-              borderWidth: 1,
-          },
-      ],
-  };
+    const handleCompare = () => {
+        if (selectedEvents.length < 2) {
+            window.alert("Select 2 events");
+            return;
+        }
+        const selectedLabels = selectedEvents.map(event => event.event_name);
+        const selectedCasualties = selectedEvents.map(event => event.casualties);
 
-  setChartData(newChartData);
-};
-
-const [chartData, setChartData] = useState({ labels: [], datasets: [] });
-useEffect(() => {
-  axios.get("http://localhost:5000/events")
-      .then(response => {
-          const fetchedEvents = response.data;
-          setEvents(fetchedEvents);
-
-          const labels = fetchedEvents.map(event => event.event_name);
-          const casualties = fetchedEvents.map(event => event.casualties);
-          const chartDatad = {
-            labels: labels,
+        const newChartData = {
+            labels: selectedLabels,
             datasets: [
                 {
                     label: 'Casualties',
-                    data: casualties,
+                    data: selectedCasualties,
                     backgroundColor: [
                         'rgba(234, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
+                        // ... you can add more colors if you have more datasets ...
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
+                        // ... corresponding border colors ...
                     ],
                     borderWidth: 1,
                 },
             ],
         };
-          setChartData(chartDatad);
-          
-          setOriginalChartData(chartDatad);
-      });   
-}, []);
 
-const resetChart = () => {
-    setChartData(originalChartData);
-}
+        setChartData(newChartData);
+        setCompareData(selectedEvents);
+    };
 
-  return (
-    <>
+    const [chartData, setChartData] = useState({ labels: [], datasets: [] });
+    useEffect(() => {
+        axios.get("http://localhost:5000/events")
+            .then(response => {
+                const fetchedEvents = response.data;
+                setEvents(fetchedEvents);
 
-        <div style={{display: 'flex', gap: '20px', marginTop: '2em'}}>
-            <div style = {{height: '20em', width: '40em'}}><BarChart chartData={chartData}/>
-            </div>
-            <div className='eventlistWrapper'>
-                <div className= "eventList">
-                    {events.map((event, index) => (
-                        <div key={index} className={`eventCard ${selectedEvents.includes(event) ? 'selected' : ''}`}
-                        onClick={() => handleEventSelection(event)}
-                        >
-                            <div className="eventDetails">
-                                <div className="eventTitle">{event.event_name}</div>
-                                <div className="eventDate">Date: {event.date}</div>
-                                <div className="eventDescription">{event.summary}</div>
-                                <div className='location'>Location: {event.location}</div>
-                                <div className="eventFooter"></div>
-                            </div>
-                        </div>))}
+                const labels = fetchedEvents.map(event => event.event_name);
+                const casualties = fetchedEvents.map(event => event.casualties);
+                const chartDatad = {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: 'Casualties',
+                            data: casualties,
+                            backgroundColor: [
+                                'rgba(234, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                            ],
+                            borderWidth: 1,
+                        },
+                    ],
+                };
+                setChartData(chartDatad);
+
+                setOriginalChartData(chartDatad);
+            });
+    }, []);
+
+    const resetChart = () => {
+        setChartData(originalChartData);
+        setSelectedEvents([]);
+        setCompareData([]);
+    }
+
+    return (
+        <>
+
+            <div style={{ display: 'flex', gap: '20px', marginTop: '2em' }}>
+                <div style={{ height: '20em', width: '40em' }}><BarChart chartData={chartData} />
                 </div>
-                <button class = "button-9" onClick={handleCompare}>Compare</button>
-                <button style = {{float: "right"}} class = "button-9" onClick={resetChart}>Reset</button>
+                <div className='eventlistWrapper'>
+                    <div className="eventList">
+                        {events.map((event, index) => (
+                            <div key={index} className={`eventCard ${selectedEvents.includes(event) ? 'selected' : ''}`}
+                                onClick={() => handleEventSelection(event)}
+                            >
+                                <div className="eventDetails">
+                                    <div className="eventTitle">{event.event_name}</div>
+                                    <div className="eventDate">Date: {event.date}</div>
+                                    <div className="eventDescription">{event.summary}</div>
+                                    <div className='location'>Location: {event.location}</div>
+                                    <div className="eventFooter"></div>
+                                </div>
+                            </div>))}
+                    </div>
+                    <button class="button-9" onClick={handleCompare}>Compare</button>
+                    <button style={{ float: "right" }} class="button-9" onClick={resetChart}>Reset</button>
+
+
+                </div>
             </div>
-        </div>
-    
-    </>
-  );
+            <div>< Compare Data={compareData} /></div>
+
+        </>
+    );
 }
 
 export default page
