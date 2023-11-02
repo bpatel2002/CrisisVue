d858cd5b6b35b3fd92b557415afcd8b2c685fd5c
bpatel21 <bpatel21@vt.edu>
2023-11-02 15:13:11 -0400

    Fully working authentication with login logout pages, backend api auth check, and conditional rendering for the navbar

--- client/frontend/src/app/components/Navbar.js
+++ client/frontend/src/app/components/Navbar.js
@@ -1,29 +1,34 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "./logo";
 import NavItem from "./NavItem";
-
+import { auth } from '../firebase';
 const MENU_LIST = [
   { text: "Home", href: "/" },
   { text: "Visualization", href: "../visual" },
-  { text: "Submit", href: "/submit" },
 ];
 const Navbar = () => {
   const [navActive, setNavActive] = useState(null);
   const [activeIdx, setActiveIdx] = useState(-1);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(newUser => {
+      setUser(newUser);
+    });
+  
+    // Make sure to unsubscribe from the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
 
   return (
     <header>
       <nav className={`nav`}>
-        <Link href={"/"}>
+        <Link href="/">
           <Logo />
         </Link>
-        <div
-          onClick={() => setNavActive(!navActive)}
-          className={`nav__menu-bar`}
-        >
+        <div onClick={() => setNavActive(!navActive)} className={`nav__menu-bar`}>
           <div></div>
           <div></div>
           <div></div>
@@ -40,6 +45,38 @@ const Navbar = () => {
               <NavItem active={activeIdx === idx} {...menu} />
             </div>
           ))}
+          {user && (
+            <>
+              {/* Submit */}
+              <div
+                onClick={() => {
+                  setActiveIdx(MENU_LIST.length); // Assuming "Submit" is next in the order
+                  setNavActive(false);
+                }}
+              >
+                <NavItem active={activeIdx === MENU_LIST.length} text="Submit" href="/submit" />
+              </div>
+              {/* Logout */}
+              <div
+                onClick={() => {
+                  setActiveIdx(MENU_LIST.length + 1); // Assuming "Logout" is after "Submit"
+                  setNavActive(false);
+                }}
+              >
+                <NavItem active={activeIdx === MENU_LIST.length + 1} text="Logout" href="/logout" />
+              </div>
+            </>
+          )}
+          {!user && (
+            <div
+              onClick={() => {
+                setActiveIdx(MENU_LIST.length); // Assuming "Login" takes the place of "Submit" when logged out
+                setNavActive(false);
+              }}
+            >
+              <NavItem active={activeIdx === MENU_LIST.length} text="Login" href="/login" />
+            </div>
+          )}
         </div>
       </nav>
     </header>
